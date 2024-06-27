bc452137b9b22ea92a30c5287dd3c554bc019cd9
Biswojit Biswal <biswojitbiswal34@gmail.com>
2024-06-27 20:43:02 +0530

    add conversion and comparision operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -39,6 +39,7 @@ let stringNumber = String(someNumber)
 let value = 3
 let negValue = -value
 // console.log(negValue);
+//  -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -52,16 +53,22 @@ let str2 = " hitesh"
 
 let str3 = str1 + str2
 // console.log(str3);
+//   hello hitesh
 
 // console.log("1" + 2);
+// 12
 // console.log(1 + "2");
+//12
 // console.log("1" + 2 + 2);
+// 122  as string comes in first position, it treats every elements as string
 // console.log(1 + 2 + "2");
-
+// 32   as number comes in first position
 // console.log( (3 + 4) * 5 % 3);
 
 // console.log(+true);
+//    1
 // console.log(+"");
+//     0
 
 let num1, num2, num3
 
--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -6,16 +6,26 @@
 
 
 // console.log("2" > 1);
+//   true
 // console.log("02" > 1);
+//   true
 
 console.log(null > 0);
+//  false
 console.log(null == 0);
+//  false
 console.log(null >= 0);
+//  true
+
+//comparison operator converts null to a number treating it as 0'
+
+
+
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// ===  this checks value as well as the datarype
 
 console.log("2" === 2);
