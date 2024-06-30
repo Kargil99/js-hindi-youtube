07a36d45ded1ff5acd2869d6fa38d90b346b8371
Biswojit Biswal <biswojitbiswal34@gmail.com>
2024-06-30 11:04:49 +0530

    Modify the comments approprately

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -14,12 +14,19 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.pop()
 
 // myArr.unshift(9)
+// add 9 to the beginning
 // myArr.shift()
+// remove the element at the beginning
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+// false
+console.log(myArr.indexOf(3));
+//3
+console.log(myArr.indexOf(34));
+// -1
 
 // const newArr = myArr.join()
+// converts the array to string
 
 // console.log(myArr);
 // console.log( newArr);
@@ -28,13 +35,18 @@ const myArr2 = new Array(1, 2, 3, 4)
 // slice, splice
 
 console.log("A ", myArr);
+// A [0, 1, 2, 3, 4, 5]
 
 const myn1 = myArr.slice(1, 3)
 
 console.log(myn1);
 console.log("B ", myArr);
-
+// B [0, 1, 2, 3, 4, 5]
 
 const myn2 = myArr.splice(1, 3)
+// splice operation manipulates the original array
 console.log("C ", myArr);
+// C [0, 4, 5]
+
 console.log(myn2);
+//[1, 2, 3]
