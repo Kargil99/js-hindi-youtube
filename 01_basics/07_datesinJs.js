66ba35eecb76263a7413b3fe7850aa749f78cf50
Biswojit Biswal <biswojitbiswal34@gmail.com>
2024-06-30 10:44:12 +0530

    add arrays and datetime

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,31 +1,35 @@
 // Dates
 
-let myDate = new Date()
+// let myDate = new Date()
 // console.log(myDate.toString());
+// //  2024-06-28T04:39:33.331Z
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
+// //28/6/2024, 10:19:45 am
 // console.log(typeof myDate);
+// //   object
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+// let myCreatedDate1 = new Date(2023, 0, 23)
+// let myCreatedDate2 = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate3 = new Date("2023-01-14")
+// let myCreatedDate4 = new Date("01-14-2023")
+// console.log(myCreatedDate1.toLocaleString());
 
-let myTimeStamp = Date.now()
+// let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
 newDate.toLocaleString('default', {
+
     weekday: "long",
     
 })
--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,6 +2,7 @@
 
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
+//   js arrays are resizable.
 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
